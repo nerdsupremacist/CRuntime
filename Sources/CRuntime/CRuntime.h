58cd7fc606a19783f93e68203697e33ea3313891
Mathias Quintero <me@quintero.io>
2020-03-01 18:03:48 +0100

    Splitting file

--- Sources/CRuntime/CRuntime.h
+++ Sources/CRuntime/CRuntime.h
@@ -14,14 +14,6 @@ const void * _Nullable swift_allocObject(
 
 void * _Nonnull callFunction(void * _Nonnull function,
                              void * _Nonnull data,
-                             int size) {
-
-    typedef void * CastedFunction(char data[size]);
-
-    char castedData[size];
-    memcpy(&castedData, data, sizeof(castedData));
-    CastedFunction *castedFunction = (CastedFunction *)(function);
-    return castedFunction(castedData);
-}
+                             int size);
 
 #endif
