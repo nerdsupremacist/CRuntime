95905c71958017a3e2ac21e72c9eb1ff9d0e8e8e
Mathias Quintero <me@quintero.io>
2020-03-01 17:31:33 +0100

    Adding callFunction function

--- Sources/CRuntime/CRuntime.h
+++ Sources/CRuntime/CRuntime.h
@@ -12,4 +12,16 @@ const void * _Nullable swift_allocObject(
                     int requiredSize,
                     int requiredAlignmentMask);
 
+void * callFunction(void *function,
+                    void *data,
+                    int size) {
+
+    typedef void * CastedFunction(u_char data[size]);
+
+    u_char castedData[size];
+    memcpy(&castedData, data, sizeof(castedData));
+    CastedFunction *castedFunction = (CastedFunction *)(function);
+    return castedFunction(castedData);
+}
+
 #endif
